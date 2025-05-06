7290a12e747c25a5986c7085694c1b6e2960eceb
Gabriel <ggoncalves.mont@gmail.com>
2025-05-06 18:23:55 -0300

    feat: register asset and mint methods

--- src/Legit.sol
+++ src/Legit.sol
@@ -1,14 +1,50 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.13;
 
-contract Legit {
-    uint256 public number;
+import {ERC721} from "solady/tokens/ERC721.sol";
+import {Ownable} from "solady/auth/Ownable.sol";
 
-    function setNumber(uint256 newNumber) public {
-        number = newNumber;
+contract Legit is ERC721, Ownable {
+    mapping(bytes => uint32) private _assetsTotalSupply;
+    mapping(bytes32 => bool) private _assetsMinted;
+
+    constructor() {
+        _initializeOwner(msg.sender);
     }
 
-    function increment() public {
-        number++;
+    function registerAsset(bytes memory assetKey, uint32 totalSupply) public {
+        require(totalSupply > 0, "Total supply must be greater than zero");
+        _assetsTotalSupply[assetKey] = totalSupply;
+    }
+
+    function mint(bytes memory assetKey, uint32 tokenId) public {
+        require(_assetsTotalSupply[assetKey] > 0, "Asset not registered");
+        require(_assetsTotalSupply[assetKey] >= tokenId, "Asset do not exist");
+
+        bytes32 assetHash = keccak256(abi.encodePacked(assetKey, tokenId));        
+        require(!_assetsMinted[assetHash], "Asset already minted");
+        
+        _mint(msg.sender, uint256(assetHash));
+        _assetsMinted[assetHash] = true;
+    }
+
+    function getOwnerOf(bytes memory assetKey, uint32 tokenId) public view returns (address) {
+        return _ownerOf(uint256(keccak256(abi.encodePacked(assetKey, tokenId))));
+    }
+
+    function getAssetTotalSupply(bytes memory assetKey) public view returns (uint32) {
+        return _assetsTotalSupply[assetKey];
+    }
+
+    function name() public pure override returns (string memory) {
+        return "Legit";
+    }
+
+    function symbol() public pure override returns (string memory) {
+        return "LGT";
+    }
+
+    function tokenURI(uint256 id) public pure override returns (string memory) {
+        return string(abi.encodePacked("https://api.example.com/metadata/", id));
     }
 }
--- test/Legit.t.sol
+++ test/Legit.t.sol
@@ -6,19 +6,65 @@ import {Legit} from "../src/Legit.sol";
 
 contract LegitTest is Test {
     Legit public legit;
+    string assetKey = "10f26e28-bfd7-4928-af1e-8948e0b6a51d";
+    uint32 totalSupply = 100;
+    address fakeSender = address(0x7FA9385bE102ac3EAc297483Dd6233D62b3e1496);
 
     function setUp() public {
         legit = new Legit();
-        legit.setNumber(0);
     }
 
-    function test_Increment() public {
-        legit.increment();
-        assertEq(legit.number(), 1);
+    function test_RegisterAsset() public {
+        legit.registerAsset(bytes(assetKey), totalSupply);
+
+        assertEq(legit.getAssetTotalSupply(bytes(assetKey)), totalSupply);
+    }
+    
+    function test_RegisterAssetWithZeroSupply() public {
+        vm.expectRevert("Total supply must be greater than zero");
+        legit.registerAsset(bytes(assetKey), 0);
     }
 
-    function testFuzz_SetNumber(uint256 x) public {
-        legit.setNumber(x);
-        assertEq(legit.number(), x);
+    function test_MintAsset() public {
+        legit.registerAsset(bytes(assetKey), totalSupply);
+
+        for (uint32 i = 1; i <= totalSupply; i++) {
+            bytes memory assetKeyHash = bytes(assetKey);
+
+            legit.mint(assetKeyHash, i);
+            assertEq(legit.getOwnerOf(assetKeyHash, i), address(this));
+        }
+    }
+
+    function test_MintAssetAlreadyMinted() public {
+        legit.registerAsset(bytes(assetKey), totalSupply);
+
+        bytes memory assetKeyHash = bytes(assetKey);
+        legit.mint(assetKeyHash, 1);
+
+        vm.expectRevert("Asset already minted");
+        legit.mint(assetKeyHash, 1);
+    }
+
+    function test_MintAssetNotRegistered() public {
+        bytes memory assetKeyHash = bytes(assetKey);
+
+        vm.expectRevert("Asset not registered");
+        legit.mint(assetKeyHash, 1);
+    }
+
+    function test_MintAssetDoesNotExist() public {
+        legit.registerAsset(bytes(assetKey), totalSupply);
+
+        bytes memory assetKeyHash = bytes(assetKey);
+
+        vm.expectRevert("Asset do not exist");
+        legit.mint(assetKeyHash, totalSupply + 1);
+    }
+
+    function test_GetTotalSupply() public {
+        legit.registerAsset(bytes(assetKey), totalSupply);
+
+        assertEq(legit.getAssetTotalSupply(bytes(assetKey)), totalSupply);
     }
 }
